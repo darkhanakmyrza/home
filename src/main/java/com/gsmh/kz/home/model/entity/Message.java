39ea9c6393ac16d7615aa1d88fe69cf5b2beeb42
Darkhan <d.akmyrza@aster-auto.kz>
2022-09-06 01:09:28 +0600

    feat: created entity messages, messages_box

---
+++ src/main/java/com/gsmh/kz/home/model/entity/Message.java
@@ -0,0 +1,42 @@
+package com.gsmh.kz.home.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(name = "messagesIdSeq", sequenceName = "messages_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "messagesIdSeq")
+    private Long id;
+
+
+    private Long adsId;
+
+    private Long fromUserId;
+    private Long toUserId;
+
+    private Boolean read;
+    private Date readDate;
+    private Long messageBoxId;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private Date createdDate;
+
+
+}
